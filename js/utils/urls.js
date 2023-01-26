96de1184642738d0f4fab4a721e4546e29f7d9bb
StorDev <>
2023-01-26 23:34:47 +0100

    Add utils: urls

---
+++ js/utils/urls.js
@@ -0,0 +1,7 @@
+const backURL = `https://skypro-rock-scissors-paper.herokuapp.com`;
+export const loginURL = `${backURL}/login`;
+export const playListURL = `${backURL}/player-list`;
+export const startURL = `${backURL}/start`;
+export const playerStatusURL = `${backURL}/player-status`;
+export const gameStatusURL = `${backURL}/game-status`;
+export const playURL = `${backURL}/play`;
